d0aff48c58cbe1cadac09c6403b58d77c38b5947
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-10-12 20:29:35 +0400

    fix wall template

--- src/jvmMain/kotlin/ru/dmitriyt/vkarchiver/data/resources/WallPostTemplates.kt
+++ src/jvmMain/kotlin/ru/dmitriyt/vkarchiver/data/resources/WallPostTemplates.kt
@@ -36,26 +36,54 @@ object WallPostTemplates {
         val formatter = SimpleDateFormat("dd.MM.yyyy HH:mm")
         val dateString = formatter.format((post.date ?: 0) * 1000L)
         return """
-        <div class="post">
+        <div class="post" id="wall${post.fromId}_${post.id}">
             <div class="post-author">Автор: ${post.fromId ?: 0}</div>
             <div class="post-date">$dateString</div>
-            <div class="post-text">${post.text ?: ""}</div>
+            <div class="post-text">${post.text?.let(::formatText) ?: ""}</div>
             <div class="post-attachments">
-                ${post.attachments.orEmpty().map { getAttachmentHtml(it) }.joinToString("\n")}
+                ${post.attachments.orEmpty().joinToString("\n") { getAttachmentHtml(it) }}
             </div>
         </div>
         <hr/>
     """.trimIndent()
     }
 
+    private const val VK_WALL_LINK_TEMPLATE = "\\[(https://vk\\.com/wall[^| ]*)\\|([^]]+)]"
+    private const val VK_LINK_TEMPLATE = "\\[(https://[^| ]*)\\|([^]]+)]"
+    private const val LINK_SPACE_TEMPLATE = " https://[^ \\n]*"
+    private const val LINK_ENTER_TEMPLATE = "[\\n]https://[^ \\n]*"
+
+    private fun formatText(text: String): String {
+        return text
+            .replace(Regex(VK_WALL_LINK_TEMPLATE)) { result ->
+                val wallPostLink = result.groupValues.getOrNull(1)?.replace("https://vk.com/", "")
+                val label = result.groupValues.getOrNull(2)
+                "<a href=\"#${wallPostLink.orEmpty()}\">${label ?: wallPostLink}</a>"
+            }
+            .replace(Regex(VK_LINK_TEMPLATE)) { result ->
+                val link = result.groupValues.getOrNull(1)
+                val label = result.groupValues.getOrNull(2)
+                "<a href=\"${link.orEmpty()}\">${label ?: link}</a>"
+            }
+            .replace(Regex(LINK_SPACE_TEMPLATE)) { result ->
+                val link = result.groupValues.getOrNull(0)?.substring(1)
+                " <a href=\"${link.orEmpty()}\">$link</a> "
+            }
+            .replace(Regex(LINK_ENTER_TEMPLATE)) { result ->
+                val link = result.groupValues.getOrNull(0)?.substring(1)
+                "\n<a href=\"${link.orEmpty()}\">$link</a> "
+            }
+            .replace("\n", "<br>")
+    }
+
     private fun getAttachmentHtml(attachment: WallpostAttachment): String {
         return when (attachment.type) {
             WallpostAttachmentType.PHOTO -> """<div class="attachment-photo">
-                <img src="${attachment.photo.sizes.maxByOrNull { it.width }?.url}"/>
-                ${attachment.photo.images.orEmpty().map { """<a href="${it.url}">${it.url}</a>""" }.joinToString("\n")}
+                <img width=300 src="${attachment.photo.sizes.maxByOrNull { it.width }?.url}"/>
+                ${attachment.photo.images.orEmpty().joinToString("\n") { """<a href="${it.url}">${it.url}</a>""" }}
             </div>""".trimIndent()
             WallpostAttachmentType.VIDEO -> """<div class="attachment-video">
-                <img src="${attachment.video.firstFrame?.maxByOrNull { it.width }?.url}"/>
+                <img width=300 src="${attachment.video.firstFrame?.maxByOrNull { it.width }?.url}"/>
                 ${attachment.video.ownerId}_${attachment.video.id}
                 ${attachment.video.trackCode}
             </div>""".trimIndent()
@@ -64,7 +92,7 @@ object WallPostTemplates {
             WallpostAttachmentType.LINK ->
                 """<div class="attachment-link"><a href="${attachment.link.url}">${attachment.link.url}</a></div>"""
             WallpostAttachmentType.PHOTOS_LIST -> """<div class="attachment-photoList">
-            ${attachment.photosList.map { """<a href="$it">$it</a>""" }.joinToString("\n")}
+            ${attachment.photosList.joinToString("\n") { """<a href="$it">$it</a>""" }}
         """.trimIndent()
             else -> ""
         }
